eb06742815bbc80e9d04d0522578b0452f1dfc60
Vitaliy <animus2393@gmail.com>
2020-04-07 20:59:14 +0300

    make something tasks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+
+
+
+
+//Task 1
 const Minutes = new Date().getMinutes();
 
 const Second = new Date().getUTCSeconds();
-console.log(Second);
 
 const inputValue = prompt('Введите значение');
-
 console.log(typeof inputValue, typeof Minutes);
 
-//Task 1
+console.log(Second);
+
 if (Minutes === 4) {
     alert('Будильник звонит');
 }
-
 
 //Task2
 switch (Minutes) {
@@ -33,23 +36,21 @@ switch (Minutes) {
 
 
 // Вариант, при котором будильник звонит во время, записанное в prompt и каждые 10 мин
-// let min = Minutes + 10;
-//
-// if (parseInt(inputValue) === min || Minutes === parseInt(inputValue)){
-//     alert("Будильник  тоже звонит");
-//
-// } else if (parseInt(inputValue) > min) {
-//     alert("Будильник уже не звонит");
-// }
-// else {
-//     alert("Ничего не происходит");
-// }
+let min = Minutes + 10;
+
+if (parseInt(inputValue) === min || Minutes === parseInt(inputValue)){
+    alert("Будильник  тоже звонит");
+
+} else if (parseInt(inputValue) > min) {
+    alert("Будильник уже не звонит");
+}
+else {
+    alert("Ничего не происходит");
+}
 
 
 //Task 3
-//С сеундами - на 59 минут 00 сек-45 сек работает. В остальное время не работает. С остальными также
-
-let  inputValueNumber = parseInt(inputValue);
+let inputValueNumber = parseInt(inputValue);
 
 
 if (inputValueNumber === 59 && Minutes === 59 && Second <= 45) {
@@ -60,14 +61,46 @@ if (inputValueNumber === 59 && Minutes === 59 && Second <= 45) {
 }
 
 
+
 //Task 4
+const currentTime = new Date().toTimeString().slice(0, 5);
+console.log(currentTime);
 
-const currentDate  = new Date().toTimeString();
-console.log(currentDate);
-
-if (parseInt(currentDate) === parseInt("23:00") && parseInt(currentDate) === inputValueNumber){
-    alert('Ага')
+if (currentTime === "17:11") {
+    alert('Время совпадает')
 }
 
 
 
+//Task 5
+const currentTimeAll = new Date().toTimeString().slice(0, 8);
+console.log(currentTimeAll);
+
+if (currentTimeAll === "23:59:59") {
+    alert('Время тоже совпадает');
+}
+
+
+//Tsk 6
+const currentDateMonth = new Date().toLocaleString().slice(0, 5);
+console.log(currentDateMonth);
+
+if (currentDateMonth === '14.01') {
+    alert('Совпадает');
+}
+
+//Второй вариант решения этой задачи
+const currentDay = new Date().getDate();
+console.log(currentDay);
+
+const currentOnlyMonth = new Date().getMonth();
+console.log(currentOnlyMonth);
+
+let TrueMonth = currentOnlyMonth + 1;
+
+let sumDateNotFull = '0' + String(currentDay) + '.' + '0' + String(TrueMonth);
+console.log(sumDateNotFull);
+
+if (sumDateNotFull === '07.04') {
+    alert('Тоже совпадает');
+}
