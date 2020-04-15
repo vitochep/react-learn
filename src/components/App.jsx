b9a488feaf355bed89123cc67893bc32cc421df7
Vitaliy <animus2393@gmail.com>
2020-04-15 16:14:43 +0300

    megre from develop branch

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as routes from 'routes/index.js';
+
+const names = [
+	'Main',
+	'Account',
+];
+
+class App extends React.Component {
+	render = () => {
+		return names.map((moduleName, i) => {
+			const Component = routes[moduleName];
+
+			return <Component key={i} />;
+		});
+	};
+};
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import Account from './Account.jsx';
+import Main from './Main.jsx';
+
+export {
+	Account,
+	Main,
+};
