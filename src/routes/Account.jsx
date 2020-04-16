795a1d7c47a65b268fc304167f7ffbf3389deef6
Vitaliy <animus2393@gmail.com>
2020-04-16 21:28:54 +0300

    process layout in react

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -6,19 +6,12 @@ class Account extends React.Component {
             <div className="body-page">
                 <div className="main-container">
                     <div className="avatar">
-
                         <div className="image-wrap">
                             <img  className='image-item' src="https://cs9.pikabu.ru/post_img/2017/05/12/8/1494592816133830021.jpg" alt="Здесь должна быть аватарка"/>
                         </div>
                     </div>
                     <div className="inputName">
                         <input className='name' type="text"/>
-                    </div>
-                    <div className="inputMail">
-                        <input className='mail' type="text"/>
-                    </div>
-                    <div className="buttonSave">
-
                     </div>
                 </div>
             </div>
