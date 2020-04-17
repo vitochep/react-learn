418188cc1ef195adbd78f0f0466e7daa4c98b6ee
Vitaliy <animus2393@gmail.com>
2020-04-17 17:32:53 +0300

    layout account

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -3,20 +3,30 @@ import React from 'react';
 class Account extends React.Component {
 	render = () => {
 		return <div>
-            <div className="body-page">
+            <div className="account-section">
+                <h1 className='account-title'>Персональные данные</h1>
                 <div className="main-container">
                     <div className="avatar">
                         <div className="image-wrap">
                             <img  className='image-item' src="https://cs9.pikabu.ru/post_img/2017/05/12/8/1494592816133830021.jpg" alt="Здесь должна быть аватарка"/>
                         </div>
                     </div>
-                    <div className="inputName">
-                        <input className='name' type="text"/>
+                    <div className="inputs-content">
+                        <div className="input-wrap">
+                            <span className='input-title'>Имя и Фамилия</span>
+                            <input className='input_item' type="text"/>
+                        </div>
+                   <div className="input-wrap">
+                       <span className='input-title'>Email</span>
+                       <input className='input_item' type="email"/>
+                   </div>
+
                     </div>
+
                 </div>
             </div>
 		</div>;
 	};
-};
+}
 
 export default Account;
