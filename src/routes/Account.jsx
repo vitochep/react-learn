fb0a393a93d72f18a330becc6ae73c886f2c1b50
Vitaliy <animus2393@gmail.com>
2020-04-17 17:58:28 +0300

    layout account

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
 
 class Account extends React.Component {
-	render = () => {
-		return <div>
+    render = () => {
+        return <div>
             <div className="account-section">
                 <h1 className='account-title'>Персональные данные</h1>
                 <div className="main-container">
                     <div className="avatar">
                         <div className="image-wrap">
-                            <img  className='image-item' src="https://cs9.pikabu.ru/post_img/2017/05/12/8/1494592816133830021.jpg" alt="Здесь должна быть аватарка"/>
+                            <img className='image-item'
+                                 src="https://cs9.pikabu.ru/post_img/2017/05/12/8/1494592816133830021.jpg"
+                                 alt="Здесь должна быть аватарка"/>
                         </div>
                     </div>
-                    <div className="inputs-content">
-                        <div className="input-wrap mod-name">
-                            <span className='input-title'>Имя и Фамилия</span>
-                            <input className='input_item' type="text"/>
+                    <div className="container-right">
+                        <div className="inputs-content">
+                            <div className="input-wrap mod-name">
+                                <span className='input-title'>Имя и Фамилия</span>
+                                <input className='input_item' type="text"/>
+                            </div>
+                            <div className="input-wrap">
+                                <span className='input-title'>Email</span>
+                                <input className='input_item' type="email"/>
+                            </div>
                         </div>
-                   <div className="input-wrap">
-                       <span className='input-title'>Email</span>
-                       <input className='input_item' type="email"/>
-                   </div>
-
+                        <form action="" className="buttonSend">
+                            Отправить
+                        </form>
                     </div>
-
                 </div>
             </div>
-		</div>;
-	};
+        </div>;
+    };
 }
 
 export default Account;
