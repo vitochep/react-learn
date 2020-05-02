ed1f7500f9deff130cb5959ee777ee982ca99639
Vitaliy <animus2393@gmail.com>
2020-05-02 22:24:13 +0300

    fix Select component

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -46,16 +46,18 @@ export default class Select extends React.Component {
                     ? this.props.children[this.state.index]
                     : this.props.children}
             </ButtonDropdown>
-            {this.state.display
-                ? isArray
-                    ? this.props
-                        .children
-                        .map((item, index) => {
-                            return <ButtonOption
-                                key={index}
+            {this.state.display   //Если текущее состояние true (равно тому значению, которое в него записано)
+                ? isArray         //И если это является массивом
+                    ? this.props  // то вывести массив {Content}
+                        .children // из City
+                        .map((item, index) => { // и перебрать его в цикле map,который вернёт
+                            return <ButtonOption // новый массив текста в виде
+                                key={index}      // {item}, который является элементом массива
                                 onClick={this.onClickItem(index)}>
                                 {item}
-                            </ButtonOption>;
+                            </ButtonOption>; //  и обрамляется в данном случае ButtonOption
+                                             //При клике на этот ButtonOption возвращается текущий элемент
+                                             // массива, тот, на котрый нажали
                         })
                     : <ButtonOption onClick={this.onClickItem(0)}>
                         {this.props.children}
@@ -65,3 +67,19 @@ export default class Select extends React.Component {
 
     }
 }
+
+// {this.state.display          Если
+//                 ? isArray
+//                     ? this.props
+//                         .children
+//                         .map((item, index) => {
+//                             return <ButtonOption
+//                                 key={index}
+//                                 onClick={this.onClickItem(index)}>
+//                                 {item}
+//                             </ButtonOption>;
+//                         })
+//                     : <ButtonOption onClick={this.onClickItem(0)}>
+//                         {this.props.children}
+//                     </ButtonOption>
+//                 : <React.Fragment />}
